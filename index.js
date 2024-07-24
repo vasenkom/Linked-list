088abcb0e38d5d1ec019f42eef2e2dfc1b96d744
vasenkom <m.vasenko@yandex.ru>
2024-07-24 09:48:53 +0200

    Rearange the order so the code not look so messy

--- index.js
+++ index.js
@@ -2,19 +2,7 @@
 function LinkedList() {
   let head = null;
 
-  // Adds a new node containing value to the end of the list
-  function addLast(value) {
-    let newNode = new Node(value);
-    if (head == null) {
-      head = newNode;
-    } else {
-      let current = head;
-      while (current.link !== null) {
-        current = current.link;
-      }
-      current.link = newNode;
-    }
-  }
+  addLast(value);
 }
 
 // Node is a factory, containing a value property and a link to the nextNode, set both as null by default
@@ -22,4 +10,18 @@ function Node(value) {
   let value;
   let link;
   return { value, link: null };
+}
+
+// Adds a new node containing value to the end of the list
+function addLast(value) {
+  let newNode = new Node(value);
+  if (head == null) {
+    head = newNode;
+  } else {
+    let current = head;
+    while (current.link !== null) {
+      current = current.link;
+    }
+    current.link = newNode;
+  }
 }
