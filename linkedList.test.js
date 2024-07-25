96a50c09862d0b83c478ff6d6bf38d557608cc43
vasenkom <m.vasenko@yandex.ru>
2024-07-25 10:35:35 +0200

    add test for prepend() and size()

--- linkedList.test.js
+++ linkedList.test.js
@@ -12,3 +12,20 @@ test("Test if nodes are being added to List", () => {
     "(dog) ->(cat) ->(parrot) ->(hamster) ->(snake) ->(turtle) -> null"
   );
 });
+
+test("test if function prepend adds a new node to the start of the list", () => {
+  const list = new LinkedList();
+  list.append("dog");
+  list.append("cat");
+  list.prepend("owl");
+  expect(list.toString()).toBe("(owl) ->(dog) ->(cat) -> null");
+});
+
+test("test if function size correctly returns the length of the list", () => {
+  const list = new LinkedList();
+  list.append("dog");
+  list.append("cat");
+  list.append("snake");
+  list.append("turtle");
+  expect(list.size()).toBe(4);
+});
