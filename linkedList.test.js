8453beb317b5e47157ed93b76afb684a62f41126
vasenkom <m.vasenko@yandex.ru>
2024-07-25 11:12:55 +0200

    add testing for head(), tail(),  at()

--- linkedList.test.js
+++ linkedList.test.js
@@ -29,3 +29,28 @@ test("test if function size correctly returns the length of the list", () => {
   list.append("turtle");
   expect(list.size()).toBe(4);
 });
+
+test("test if function size correctly returns the length of the list if no nodes are added", () => {
+  const list = new LinkedList();
+  expect(list.size()).toBe(null);
+});
+
+test("test if function head returns the first node in the list and tail returns the last node in the list", () => {
+  const list = new LinkedList();
+  list.append("dog");
+  list.append("cat");
+  list.append("snake");
+  list.append("turtle");
+  expect(list.head()).toBe("dog");
+  expect(list.tail()).toBe("turtle");
+});
+
+test("test if at(index) returns the node at the given index", () => {
+  const list = new LinkedList();
+  list.append("cake");
+  list.append("pizza");
+  list.append("pasta");
+  list.append("salad");
+  expect(list.at(1)).toBe("pizza");
+  expect(list.at(0)).toBe("cake");
+});
